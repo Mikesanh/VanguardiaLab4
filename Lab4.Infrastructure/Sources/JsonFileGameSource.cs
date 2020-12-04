c360d645fdebbb83cc325fa91873bcd83f0e69b7
Michael Sanchez <sanchezmi164@gmail.com>
2020-12-03 23:43:07 -0600

    .

--- Lab4.Infrastructure/Sources/JsonFileGameSource.cs
+++ Lab4.Infrastructure/Sources/JsonFileGameSource.cs
@@ -4,13 +4,14 @@ using System.IO;
 using System.Text;
 using Lab4.Core.Interfaces;
 
+
 namespace Lab4.Infrastructure.Sources
 {
     public class JsonFileGameSource : IChessSource
     {
         public string GetDataFromJson()
         {
-            return File.ReadAllText("ChessGame.json");
+            return File.ReadAllText("C:\\Users\\sanch\\source\\repos\\Lab4\\Lab4\\ChessGame.json");
         }
 
     }
